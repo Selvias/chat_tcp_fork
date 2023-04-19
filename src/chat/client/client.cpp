b83111b1cacd8402dcf75b50e3daf186ecde9846
Selvias <selv.post@gmail.com>
2023-04-20 02:50:32 +0700

    Add basic chat functionality.

--- src/chat/client/client.cpp
+++ src/chat/client/client.cpp
@@ -5,16 +5,21 @@
 #include <thread>
 #include <unistd.h>
 
+namespace client {
 const int MAX_MESSAGE_SIZE = 1024;
+const int MAX_USERNAME_LEN = 20;
 
-int client_socket;
+int new_socket;
 char message[MAX_MESSAGE_SIZE];
+std::string full_message;
+int is_it_me = 1;
+pthread_mutex_t mutex;
 
 void *receive_messages(void *arg) {
   int bytes_read;
   while (true) {
     // Receive messages from the server
-    bytes_read = read(client_socket, message, MAX_MESSAGE_SIZE);
+    bytes_read = read(new_socket, message, MAX_MESSAGE_SIZE);
     if (bytes_read == 0) {
       std::cout << "Server closed the connection" << std::endl;
       break;
@@ -22,26 +27,44 @@ void *receive_messages(void *arg) {
       perror("Error reading from socket");
       break;
     } else {
+      if (is_it_me) {
+        is_it_me = false;
+        continue;
+      }
       message[bytes_read] = '\0';
-      std::cout << "server: " << message << std::endl;
+      std::cout << message << std::endl;
     }
   }
   pthread_exit(NULL);
 }
 
 void *send_messages(void *arg) {
+  char username[MAX_USERNAME_LEN];
+  memset(username, 0, MAX_USERNAME_LEN);
+  std::cout << "Enter your username: ";
+  std::cin >> username;
+  std::cin.get();
+  send(new_socket, username, MAX_USERNAME_LEN, 0);
   while (true) {
     // Get input from the user
+    is_it_me = false;
     if (fgets(message, MAX_MESSAGE_SIZE, stdin) != 0) {
+      is_it_me = true;
       // Send the message to the server
-      if ((send(client_socket, message, strlen(message), 0)) < 0) {
+      full_message += username;
+      full_message += ": ";
+      full_message += message;
+      if ((send(new_socket, full_message.c_str(), full_message.length(), 0)) <
+          0) {
         perror("Error sending message");
         break;
       }
+      full_message = "";
     }
   }
   pthread_exit(NULL);
 }
+} // namespace client
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -58,13 +81,12 @@ int main(int argc, char *argv[]) {
     std::cerr << "Error: invalid IP address " << ip_address_str << std::endl;
     exit(1);
   }
-  // int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-  client_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (client_socket < 0) {
+  // int new_socket = socket(AF_INET, SOCK_STREAM, 0);
+  client::new_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (client::new_socket < 0) {
     std::cerr << "Error creating socket" << std::endl;
     return 1;
   }
-  std::cout << "My socket: " << client_socket << std::endl;
 
   // Configure the server address
   struct sockaddr_in server_address;
@@ -73,7 +95,7 @@ int main(int argc, char *argv[]) {
   server_address.sin_addr = ip_address;
   server_address.sin_port = htons(port);
 
-  if ((connect(client_socket, (struct sockaddr *)&server_address,
+  if ((connect(client::new_socket, (struct sockaddr *)&server_address,
                sizeof(server_address))) == -1) {
     perror("connect");
     exit(1);
@@ -81,13 +103,14 @@ int main(int argc, char *argv[]) {
 
   pthread_t receive_thread, send_thread;
   // Start the receive_messages thread
-  if (pthread_create(&receive_thread, NULL, receive_messages, NULL) < 0) {
+  if (pthread_create(&receive_thread, NULL, client::receive_messages, NULL) <
+      0) {
     perror("Error creating receive thread");
     exit(EXIT_FAILURE);
   }
 
   // Start the send_messages thread
-  if (pthread_create(&send_thread, NULL, send_messages, NULL) < 0) {
+  if (pthread_create(&send_thread, NULL, client::send_messages, NULL) < 0) {
     perror("Error creating send thread");
     exit(EXIT_FAILURE);
   }
@@ -97,148 +120,6 @@ int main(int argc, char *argv[]) {
   pthread_join(send_thread, NULL);
 
   // Close the client socket and exit
-  close(client_socket);
+  close(client::new_socket);
   pthread_exit(NULL);
 }
-
-// #include <stdio.h>
-// #include <sys/socket.h>
-// #include <sys/types.h>
-
-// #include <errno.h>
-// #include <netdb.h>
-// #include <netinet/in.h>
-
-// #include <arpa/inet.h>
-// #include <iostream>
-// #include <strings.h>
-
-// #include <fcntl.h> // для функции fcntl()
-// #include <unistd.h>
-// #include <vector>
-
-// #include <signal.h>
-// #include <sys/wait.h>
-
-// #include <cstring>
-// #include <fstream>
-// #include <pthread.h>
-
-// #define MAX_CLIENTS 10
-// #define MAX_MESSAGE_LENGTH 1024
-// #define MAX_NAME_LENGTH 20
-// // ver 5
-// int main(int argc, char **argv) {
-
-//   int server_socket;
-//   sockaddr_in server_address;
-//   hostent *hp;
-
-//   char buffer[MAX_MESSAGE_LENGTH];
-//   ssize_t message_size;
-
-//   if (argc != 3) {
-//     printf("Error: enter server's Port and your Username!\n");
-//     exit(1);
-//   }
-
-//   // Создаем сокет
-//   if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-//     std::cerr << "Error creating socket\n";
-//     return 1;
-//   }
-
-//   bzero((char *)&server_address, sizeof(server_address));
-
-//   server_address.sin_family = AF_INET;
-
-//   hp = gethostbyname("localhost");
-
-//   bcopy(hp->h_addr, &server_address.sin_addr, hp->h_length);
-
-//   server_address.sin_port = htons(atoi(argv[1]));
-
-//   if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
-//     std::cerr << "Error invalid address\n";
-//     return 1;
-//   }
-
-//   // Подключаемся к серверу
-//   if (connect(server_socket, (sockaddr *)&server_address,
-//               sizeof(server_address)) < 0) {
-//     std::cerr << "Error connecting to server\n";
-//     return 1;
-//   }
-
-//   // Получаем приветственное сообщение
-//   // if ((message_size = recv(server_socket, buffer, MAX_MESSAGE_LENGTH, 0))
-//   <=
-//   //     0) {
-//   //   std::cerr << "Error receiving message\n";
-//   //   return 1;
-//   // }
-
-//   // buffer[message_size] = '\0';
-//   // std::cout << buffer << std::endl << std::endl;
-
-//   std::string name = argv[2];
-//   // send(server_socket, name.c_str(), name.size(), 0);
-
-//   // Отправляем сообщения серверу
-//   while (true) {
-
-//     std::string message;
-//     std::string input_message;
-
-//     fd_set read_fds;
-
-//     struct timeval timeout;
-
-//     FD_ZERO(&read_fds);
-//     FD_SET(server_socket, &read_fds);
-//     FD_SET(STDIN_FILENO, &read_fds);
-
-//     timeout.tv_sec = 0;
-//     timeout.tv_usec = 1000;
-
-//     int activity = select(server_socket + 1, &read_fds, NULL, NULL,
-//     &timeout);
-
-//     if (activity < 0) {
-//       std::cerr << "Error selecting\n";
-//       return 1;
-//     }
-
-//     if (activity == 0)
-//       continue;
-
-//     if (FD_ISSET(server_socket, &read_fds)) {
-//       if ((message_size = recv(server_socket, buffer, MAX_MESSAGE_LENGTH, 0))
-//       <=
-//           0) {
-//         std::cerr << "Error receiving message\n";
-//         return 1;
-//       }
-
-//       buffer[message_size] = '\0';
-//       std::cout << buffer << std::endl << std::endl;
-//     }
-
-//     if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-//       std::getline(std::cin, input_message);
-//       std::cout << std::endl;
-
-//       if (input_message == "/quit") {
-//         break;
-//       }
-
-//       message = name + ": " + input_message;
-//       send(server_socket, message.c_str(), message.size(), 0);
-//     }
-//   }
-
-//   // Закрываем соединение
-//   close(server_socket);
-
-//   return 0;
-// }
--- src/chat/server/server_q.cpp
+++ src/chat/server/server_q.cpp
@@ -9,17 +9,40 @@
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
-using namespace std;
+#include <vector>
 
 #define PORT 8080
 #define MAX_CLIENTS 10
 #define SHM_SIZE (MAX_CLIENTS * sizeof(int))
+#define MAX_NAME_LENGTH 20
 
 struct message {
   long type;
-  char text[256];
+  char text[1024];
 };
+
+struct client_data {
+  char name[20];
+  int is_just_connected = 0;
+  int is_just_leave = 0;
+};
+
+// broadcast data
+int msgid;
+key_t key;
+message msg;
+
+// shared data (logic)
+int *client_fds;
+int *client_lost_cnnct;
+client_data *client_dt;
+
+int server_fd, new_socket, shmid, shmid_lost, shmid_clients;
+struct sockaddr_in address;
+int opt = 1;
+int addrlen = sizeof(address);
+
+std::vector<int> shmid_list;
 
 void reaper(int signal) {
   int status;
@@ -27,12 +50,25 @@ void reaper(int signal) {
     ;
 }
 
-int msgid;
-key_t key;
-int *client_fds;
-message msg;
+void cleanup(int signal) {
+  // detach and remove shared memory segments
+  // shmdt(clients);
+  shmdt(client_fds);
+  // shmdt(client_lost_cnnct);
+  for (const int &shmid : shmid_list) {
+    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+      std::cerr << "Failed to remove shared memory segment with shmid " << shmid
+                << std::endl;
+    } else {
+      std::cout << "Removed shared memory segment with shmid " << shmid
+                << std::endl;
+    }
+  }
+  exit(0);
+}
 
-void *receive_messages(void *arg) {
+void *broadcast(void *arg) {
+  std::string notification = "";
   while (true) {
     if (msgrcv(msgid, &msg, sizeof(msg.text), 1, IPC_NOWAIT) == -1) {
       // No new messages in queue
@@ -43,116 +79,185 @@ void *receive_messages(void *arg) {
     // Broadcast message to all connected clients
     for (int i = 0; i < MAX_CLIENTS; i++) {
       if (client_fds[i] != 0) {
+        // just connected broadcast notifications
+        if (client_dt[i].is_just_connected == 1) {
+          notification += client_dt[i].name;
+          notification += " joined the chat...";
+          for (int j = 0; j < MAX_CLIENTS; j++) {
+            if (client_fds[j] != 0 && i != j) {
+              send(client_fds[j], notification.c_str(), notification.length(),
+                   0);
+            }
+          }
+          client_dt[i].is_just_connected = 0;
+          notification = "";
+        }
+        // simple message broadcast
         if (send(client_fds[i], msg.text, strlen(msg.text), 0) == -1) {
           std::cerr << "Failed to send message to client\n";
         }
+      }
+      // just leaved broadcast notifications
+      if (client_dt[i].is_just_leave == 1) {
+        notification += client_dt[i].name;
+        notification += " left the chat...";
+        for (int j = 0; j < MAX_CLIENTS; j++) {
+          if (client_fds[j] != 0 && i != j) {
+            send(client_fds[j], notification.c_str(), notification.length(), 0);
+          }
+        }
+        client_dt[i].is_just_leave = 0;
+        notification = "";
       }
     }
   }
   pthread_exit(NULL);
 }
-int server_fd, new_socket, shmid;
-struct sockaddr_in address;
-int opt = 1;
-int addrlen = sizeof(address);
+
+int add_client(int client_socket, char *username) {
+  int i;
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (client_fds[i] == 0) {
+      client_fds[i] = client_socket;
+      strcpy(client_dt[i].name, username);
+      client_dt[i].is_just_connected = 1;
+      break;
+    }
+  }
+  if (i == MAX_CLIENTS) {
+    std::cerr << "Max clients reached\n";
+    // close(client_socket);
+    return 1;
+  }
+  return 0;
+}
+
+int clear_client() {
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (client_lost_cnnct[i] == -1 && client_fds[i] != 0) {
+      // if (client_dt[i].is_connected == false && client_fds[i] != 0) {
+      std::cout << "close connection with descriptor: " << client_fds[i]
+                << std::endl;
+      // close(client_fds[i]);
+      client_lost_cnnct[i] = 0;
+      client_dt[i].is_just_leave = 1;
+      // client_dt[i].is_connected = true;
+      client_fds[i] = 0;
+      return 0;
+    }
+  }
+  return 1;
+}
 
 void *handle_clients(void *arg) {
+  // int new_socket;
+  client_lost_cnnct = (int *)shmat(shmid_lost, NULL, 0);
+  client_dt = (client_data *)shmat(shmid_clients, NULL, 0);
   signal(SIGCHLD, reaper);
+  signal(SIGINT, cleanup);
   while (true) {
     // Accept new connection
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                              (socklen_t *)&addrlen)) < 0) {
-      cerr << "Accept error\n";
+      std::cerr << "Connection accept error.\n";
       exit(EXIT_FAILURE);
     }
 
     std::cout << "new_client: " << new_socket << std::endl;
 
+    // Get username
+    char username[MAX_NAME_LENGTH];
+    int recieved_bytes = recv(new_socket, username, MAX_NAME_LENGTH - 1, 0);
+    username[recieved_bytes] = '\0';
+
     // Add new client to list of connected clients
-    int i;
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-      if (client_fds[i] == 0) {
-        client_fds[i] = new_socket;
-        break;
-      }
-    }
-    if (i == MAX_CLIENTS) {
-      cerr << "Max clients reached\n";
-      close(new_socket);
+    int is_max = add_client(new_socket, username);
+    if (is_max) {
       continue;
     }
 
     // Fork new process to handle client messages
     pid_t pid = fork();
     if (pid == 0) {
-      // Child process reads messages from client and broadcasts to all other
-      // clients
+      client_lost_cnnct = (int *)shmat(shmid_lost, NULL, 0);
+      client_dt = (client_data *)shmat(shmid_clients, NULL, 0);
       close(server_fd);
+      // recieve_messages(new_socket);
       while (true) {
         int nbytes = recv(new_socket, msg.text, sizeof(msg.text), 0);
         msg.text[nbytes] = '\0';
-        std::cout << "recieved_msg: " << msg.text << std::endl;
+        //   // std::cout << "recieved_msg: " << msg.text << std::endl;
         if (nbytes == 0) {
-          // Client disconnected
+          //     // Client disconnected
           for (int j = 0; j < MAX_CLIENTS; j++) {
             if (client_fds[j] == new_socket) {
-              client_fds[j] = 0;
-              break;
+              client_lost_cnnct[j] = -1;
             }
+            // clear_client();
           }
-          break;
         } else if (nbytes < 0) {
           // Error reading from client
-          cerr << "Read error\n";
+          std::cerr << "Read error\n";
         }
         msg.type = 1;
+        // send to parent process
         if (msgsnd(msgid, &msg, sizeof(msg.text), 0) == -1) {
           std::cerr << "Failed to send message to queue\n";
         }
-        // exit(EXIT_FAILURE);
       }
-      //   for (int j = 0; j < MAX_CLIENTS; j++) {
-      //     if (client_fds[j] != 0 && client_fds[j] != new_socket) {
-      //       if (send(client_fds[j], msg.text, strlen(msg.text), 0) < 0) {
-      //         // Error sending message to client
-      //         cerr << "Send error\n";
-      //         exit(EXIT_FAILURE);
-      //       }
-      //     }
-      //   }
+      exit(0);
     } else if (pid < 0) {
       // Fork error
-      cerr << "Fork error\n";
+      std::cerr << "Fork error\n";
       exit(EXIT_FAILURE);
     } else {
       // Parent process continues to accept new connections
-      //   close(new_socket);
+      // close all connections
     }
+  }
+}
+// close fd
+void *handle_lost_connection(void *arg) {
+  while (true) {
+    clear_client();
   }
 }
 
 int main() {
 
+  // int clnts_shmid;
   // Create shared memory segment for client file descriptors
   key = ftok("server", 'R');
   shmid = shmget(key, SHM_SIZE, 0666 | IPC_CREAT);
   client_fds = (int *)shmat(shmid, NULL, 0);
   memset(client_fds, 0, SHM_SIZE);
+  shmid_list.push_back(shmid);
 
   // Create message queue for broadcasting messages
   key = ftok("server", 'B');
   msgid = msgget(key, 0666 | IPC_CREAT);
 
+  // Create shared memory segment for lost connection clients
+  shmid_lost = shmget(IPC_PRIVATE, sizeof(int) * MAX_CLIENTS, IPC_CREAT | 0666);
+  // memset(client_lost_cnnct, 0, SHM_SIZE);
+  shmid_list.push_back(shmid_lost);
+  client_lost_cnnct = (int *)shmat(shmid_lost, NULL, 0);
+
+  shmid_clients =
+      shmget(IPC_PRIVATE, sizeof(client_data) * MAX_CLIENTS, IPC_CREAT | 0666);
+  shmid_list.push_back(shmid_clients);
+  client_dt = (client_data *)shmat(shmid_clients, NULL, 0);
+
   // Create server socket
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    cerr << "Socket creation error\n";
+    std::cerr << "Socket creation error\n";
     exit(EXIT_FAILURE);
   }
 
   // Set server socket options
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
                  sizeof(opt))) {
-    cerr << "Setsockopt error\n";
+    std::cerr << "Setsockopt error\n";
     exit(EXIT_FAILURE);
   }
 
@@ -161,19 +266,19 @@ int main() {
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    cerr << "Bind error\n";
+    std::cerr << "Bind error\n";
     exit(EXIT_FAILURE);
   }
 
   // Start listening for connections
   if (listen(server_fd, MAX_CLIENTS) < 0) {
-    cerr << "Listen error\n";
+    std::cerr << "Listen error\n";
     exit(EXIT_FAILURE);
   }
 
-  pthread_t receive_thread, handle_cl_thread;
-  // Start the receive_messages thread
-  if (pthread_create(&receive_thread, NULL, receive_messages, NULL) < 0) {
+  pthread_t bcast_thread, handle_cl_thread, check_lost_connections_thread;
+  // Start the broadcast thread
+  if (pthread_create(&bcast_thread, NULL, broadcast, NULL) < 0) {
     perror("Error creating receive thread");
     exit(EXIT_FAILURE);
   }
@@ -181,109 +286,114 @@ int main() {
     perror("Error creating receive thread");
     exit(EXIT_FAILURE);
   }
+  if (pthread_create(&check_lost_connections_thread, NULL,
+                     handle_lost_connection, NULL) < 0) {
+    perror("Error creating receive thread");
+    exit(EXIT_FAILURE);
+  }
   //   Wait for the threads to finish
-  pthread_join(receive_thread, NULL);
+  pthread_join(check_lost_connections_thread, NULL);
+  pthread_join(bcast_thread, NULL);
   pthread_join(handle_cl_thread, NULL);
 
-  //   signal(SIGCHLD, reaper);
-  //   while (true) {
-  //     // Accept new connection
-  //     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-  //                              (socklen_t *)&addrlen)) < 0) {
-  //       cerr << "Accept error\n";
-  //       exit(EXIT_FAILURE);
-  //     }
+  signal(SIGINT, cleanup);
+  signal(SIGCHLD, reaper);
+  // while (true) {
+  //   // Accept new connection
+  //   if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+  //                            (socklen_t *)&addrlen)) < 0) {
+  //     std::cerr << "Connection accept failure.\n";
+  //     exit(EXIT_FAILURE);
+  //   }
+  //   std::cout << "new user: " << new_socket << std::endl;
+  //   add_client(new_socket);
+  //   // std::cout << "connected\n";
 
-  //     std::cout << "new_client: " << new_socket << std::endl;
+  //   // Add new client to list of connected clients
+  //   // int i;
+  //   // for (int i = 0; i < MAX_CLIENTS; i++) {
+  //   //   if (client_fds[i] == 0) {
+  //   //     client_fds[i] = new_socket;
+  //   //     break;
+  //   //   }
+  //   // }
+  //   // if (i == MAX_CLIENTS) {
+  //   //   std::cerr << "Max clients reached\n";
+  //   //   close(new_socket);
+  //   //   continue;
+  //   //   // char username[20];
+  //   //   // recv(new_socket, username, 20, 0);
+  //   //   // add_client(new_socket, username);
+  //   //   // std::cout << username << " join the chat (" << new_socket << ")"
+  //   //   //           << std::endl;
+  //   // }
 
-  //     // Add new client to list of connected clients
-  //     int i;
-  //     for (int i = 0; i < MAX_CLIENTS; i++) {
-  //       if (client_fds[i] == 0) {
-  //         client_fds[i] = new_socket;
-  //         break;
-  //       }
-  //     }
-  //     if (i == MAX_CLIENTS) {
-  //       cerr << "Max clients reached\n";
-  //       close(new_socket);
-  //       continue;
-  //     }
-
-  //     // Fork new process to handle client messages
-  //     pid_t pid = fork();
-  //     if (pid == 0) {
-  //       // Child process reads messages from client and broadcasts to all
-  //       other
-  //       // clients
-  //       close(server_fd);
-  //       while (true) {
-  //         int nbytes = recv(new_socket, msg.text, sizeof(msg.text), 0);
-  //         msg.text[nbytes] = '\0';
-  //         std::cout << "recieved_msg: " << msg.text << std::endl;
-  //         if (nbytes == 0) {
-  //           // Client disconnected
-  //           for (int j = 0; j < MAX_CLIENTS; j++) {
-  //             if (client_fds[j] == new_socket) {
-  //               client_fds[j] = 0;
-  //               break;
-  //             }
+  //   // Fork new process to handle client messages
+  //   pid_t pid = fork();
+  //   if (pid == 0) {
+  //     close(server_fd);
+  //     while (true) {
+  //       int nbytes = recv(new_socket, msg.text, sizeof(msg.text), 0);
+  //       msg.text[nbytes] = '\0';
+  //       // std::cout << "recieved_msg: " << msg.text << std::endl;
+  //       if (nbytes == 0) {
+  //         // Client disconnected
+  //         for (int j = 0; j < MAX_CLIENTS; j++) {
+  //           if (client_fds[j] == new_socket) {
+  //             // client_fds[j] = 0;
+  //             client_lost_cnnct[j] = -1;
+  //             // break;
   //           }
-  //           break;
-  //         } else if (nbytes < 0) {
-  //           // Error reading from client
-  //           cerr << "Read error\n";
   //         }
-  //         msg.type = 1;
-  //         if (msgsnd(msgid, &msg, sizeof(msg.text), 0) == -1) {
-  //           std::cerr << "Failed to send message to queue\n";
-  //         }
-  //         // exit(EXIT_FAILURE);
+  //         // break;
+  //       } else if (nbytes < 0) {
+  //         // Error reading from client
+  //         std::cerr << "Read error\n";
   //       }
-  //       //   for (int j = 0; j < MAX_CLIENTS; j++) {
-  //       //     if (client_fds[j] != 0 && client_fds[j] != new_socket) {
-  //       //       if (send(client_fds[j], msg.text, strlen(msg.text), 0) < 0)
-  //       {
-  //       //         // Error sending message to client
-  //       //         cerr << "Send error\n";
-  //       //         exit(EXIT_FAILURE);
-  //       //       }
-  //       //     }
-  //       //   }
-  //     } else if (pid < 0) {
-  //       // Fork error
-  //       cerr << "Fork error\n";
-  //       exit(EXIT_FAILURE);
-  //     } else {
-  //       // Parent process continues to accept new connections
-  //       //   close(new_socket);
-  //     }
-  //   }
-
-  //   while (true) {
-  //     if (msgrcv(msgid, &msg, sizeof(msg.text), 1, IPC_NOWAIT) == -1) {
-  //       // No new messages in queue
-  //       usleep(100000);
-  //       continue;
-  //     }
-
-  //     // Broadcast message to all connected clients
-  //     for (int i = 0; i < MAX_CLIENTS; i++) {
-  //       if (client_fds[i] != 0) {
-  //         if (send(client_fds[i], msg.text, strlen(msg.text), 0) == -1) {
-  //           std::cerr << "Failed to send message to client\n";
-  //         }
+  //       msg.type = 1;
+  //       if (msgsnd(msgid, &msg, sizeof(msg.text), 0) == -1) {
+  //         std::cerr << "Failed to send message to queue\n";
   //       }
+  //       // exit(EXIT_FAILURE);
   //     }
+  //     //   for (int j = 0; j < MAX_CLIENTS; j++) {
+  //     //     if (client_fds[j] != 0 && client_fds[j] != new_socket) {
+  //     //       if (send(client_fds[j], msg.text, strlen(msg.text), 0) <
+  //     // 0)
+  //     //   exit(0);
+  //     // {
+  //     //         // Error sending message to client
+  //     //         cerr << "Send error\n";
+  //     //         exit(EXIT_FAILURE);
+  //     //       }
+  //     //     }
+  //     //   }
+  //     // close(new_socket);
+  //   } else if (pid < 0) {
+  //     // Fork error
+  //     std::cerr << "Fork error\n";
+  //     exit(EXIT_FAILURE);
+  //   } else {
+  //     // Parent process continues to accept new connections
+  //     //   close(new_socket);
   //   }
+  // }
 
   // Detach and remove shared memory segment
   shmdt(client_fds);
   shmctl(shmid, IPC_RMID, NULL);
+  shmctl(shmid_lost, IPC_RMID, NULL);
 
   // Remove message queue
   msgctl(msgid, IPC_RMID, NULL);
   pthread_exit(NULL);
+
+  // close all opened connections
+  // for (int i = 0; i < MAX_CLIENTS; i++) {
+  //   if (client_fds[i] != 0) {
+  //     close(client_fds[i]);
+  //   }
+  // }
 
   return 0;
 }
